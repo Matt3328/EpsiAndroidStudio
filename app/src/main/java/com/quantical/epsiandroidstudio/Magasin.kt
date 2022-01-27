4ae201749bf6681239c3d332ea200fb4b0e26ea6
XTAO123456 <91958477+XTAO123456@users.noreply.github.com>
2022-01-27 16:21:04 +0100

    créer les fichiers

---
+++ app/src/main/java/com/quantical/epsiandroidstudio/Magasin.kt
@@ -0,0 +1,4 @@
+package com.quantical.epsiandroidstudio
+
+class Magasin {
+}
---
+++ app/src/main/java/com/quantical/epsiandroidstudio/MagasinDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.quantical.epsiandroidstudio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MagasinDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_magasin_detail)
+    }
+}
