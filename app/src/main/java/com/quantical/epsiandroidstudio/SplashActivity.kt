232f4083c6c35d8d5a54ef584c12b61b1ef85e08
XTAO123456 <91958477+XTAO123456@users.noreply.github.com>
2022-01-30 18:57:14 +0100

    add Splash page

--- app/src/main/java/com/quantical/epsiandroidstudio/SplashActivity.kt
+++ app/src/main/java/com/quantical/epsiandroidstudio/SplashActivity.kt
@@ -3,9 +3,18 @@ package com.quantical.epsiandroidstudio
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import android.content.Intent
+import android.os.Handler
+import android.os.Looper
+
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        Handler(Looper.getMainLooper()).postDelayed(Runnable {
+            val newIntent= Intent(application,InscriptionActivity::class.java)
+            startActivity(newIntent)
+            finish()
+        },2000)
     }
 }
