e17322e1ff38e65773b77be09d6eb56d77cb131b
XTAO123456 <91958477+XTAO123456@users.noreply.github.com>
2022-01-30 18:30:07 +0100

    add functions header

--- app/src/main/java/com/quantical/epsiandroidstudio/BaseActivity.kt
+++ app/src/main/java/com/quantical/epsiandroidstudio/BaseActivity.kt
@@ -1,11 +1,32 @@
 package com.quantical.epsiandroidstudio
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 
-class BaseActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_base)
+open class BaseActivity : AppCompatActivity() {
+    fun showBtnBack(){
+        val imageViewBack=findViewById<ImageView>(R.id.imageViewBack)
+        imageViewBack.visibility= View.VISIBLE
+        imageViewBack.setOnClickListener(View.OnClickListener {
+            finish()
+        })
+    }
+
+    fun setHeaderTitle(txt : String){
+        val textViewTitle = findViewById<TextView>(R.id.textViewTitle)
+        textViewTitle.text = txt
+    }
+
+    fun showBtnProfil(){
+        val imageViewAvatar=findViewById<ImageView>(R.id.imageViewAvatar)
+        imageViewAvatar.visibility= View.VISIBLE
+        imageViewAvatar.setOnClickListener(View.OnClickListener {
+            val newIntent= Intent(application,CompteDetailActivity::class.java)
+            startActivity(newIntent)
+        })
     }
 }
