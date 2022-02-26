aadc4b1d73161ba277b28ef8e292844b23529439
Matthieu Pacheco <mattpacheco33@gmail.com>
2022-02-26 18:18:12 +0100

    qrcode

--- app/src/main/java/com/quantical/epsiandroidstudio/SplashActivity.kt
+++ app/src/main/java/com/quantical/epsiandroidstudio/SplashActivity.kt
@@ -6,13 +6,14 @@ import android.os.Bundle
 import android.content.Intent
 import android.os.Handler
 import android.os.Looper
+import com.google.example.mlkit.ScannerActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         Handler(Looper.getMainLooper()).postDelayed(Runnable {
-            val newIntent= Intent(application,MainActivity::class.java)
+            val newIntent= Intent(application, ScannerActivity::class.java)
             startActivity(newIntent)
             finish()
         },2000)
